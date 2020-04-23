5fb4600f3584dabffba1499744ee7875c3d48be0
codingmr <codingmroberts@gmail.com>
2020-04-23 19:44:28 +0100

    fixed tests with passing props in react navigation 5 with RN hooks

--- src/containers/__tests__/Screens-test.js
+++ src/containers/__tests__/Screens-test.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { HomeScreen, AboutScreen, SplashScreen } from '../../containers';
+import HomeNavigator from '../../navigation/HomeNavigator';
 import { cleanup, render } from 'react-native-testing-library';
 
 describe('Snapshot testing of screens', () => {
   afterEach(cleanup); //Unmounts React trees that were mounted with render to prevent memory leak.
   it('HomeScreen: should render correctly', () => {
-    const { toJSON } = render(<HomeScreen />);
+    const { toJSON } = render(
+      <HomeNavigator
+        route={{ name: 'Home' }}
+        component={HomeScreen}
+        tabBarIconName={'md-home'}
+      />,
+    );
     expect(toJSON()).toMatchSnapshot();
   });
   it('AboutScreen: should render correctly', () => {
-    const { toJSON } = render(<AboutScreen />);
+    const { toJSON } = render(
+      <HomeNavigator
+        route={{ name: 'About' }}
+        component={AboutScreen}
+        tabBarIconName={'md-information'}
+        params={{ data: 'Fake data' }}
+      />,
+    );
     expect(toJSON()).toMatchSnapshot();
   });
   it('SplashScreen: should render correctly', () => {
